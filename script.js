7c8fc0f71706ebe85edb01072793f73bed8974f1
GunnerSupanut <gunsupm@gmail.com>
2025-03-10 17:09:15 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () { //รอให้ HTML โหลดเสร็จก่อน แล้วค่อยทำงาน JavaScript
     const aboutSec = document.getElementById("About"); //ดึง section ที่มี id="About" มาเก็บไว้
 
-    const observer = new IntersectionObserver(entries => {//เช็คว่า Element นี้อยู่ในหน้าจอไหม
+    const Aboutbg = new IntersectionObserver(entries => {//เช็คว่า Element นี้อยู่ในหน้าจอไหม
         entries.forEach(entry => { // วนลูปผ่านแต่ละ Element ที่กำลังถูกสังเกต
             if (entry.isIntersecting) { //เป็น true ถ้า #About ปรากฏในหน้าจอ
                 document.body.style.backgroundColor = "#e1f5fe"; // เปลี่ยนสี #About
@@ -11,25 +11,22 @@ document.addEventListener("DOMContentLoaded", function () { //รอให้ HT
         });
     }, { threshold: 0.7 });
 
-    observer.observe(aboutSec);
-});
-//Hamburgerbar ในโทรศัพท์
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navList = document.querySelector(".nav-list");
-
-    // กด Hamburger เพื่อเปิด-ปิดเมนู
-    hamburger.addEventListener("click", function () {
-        hamburger.classList.toggle("active");
-        navList.classList.toggle("active");
+    const HomeSec = document.getElementById("Home"); //ดึง section ที่มี id="..." มาเก็บไว้
+    const Homebg = new IntersectionObserver(entries => {//เช็คว่า Element นี้อยู่ในหน้าจอไหม
+    entries.forEach(entry => { // วนลูปผ่านแต่ละ Element ที่กำลังถูกสังเกต
+        if (entry.isIntersecting) { //เป็น true ถ้า #... ปรากฏในหน้าจอ
+            document.body.style.backgroundColor = "#ECD4D4"; // เปลี่ยนสี #...
+        } else { 
+            cument.body.style.backgroundColor = "#ffffff";// กลับเป็นสี...เมื่อออกจาก #...
+        }
     });
+}, { threshold: 0.3 });
 
-    // กดลิงก์ในเมนูแล้วให้ปิดเมนู
-    document.querySelectorAll(".nav-list a").forEach(link => {
-        link.addEventListener("click", function () {
-            hamburger.classList.remove("active");
-            navList.classList.remove("active");
-        });
-    });
+Homebg.observe(HomeSec);
+Aboutbg.observe(aboutSec);
 });
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('#header .nav-list ul').classList.toggle('active');
+});
+
 
